4c424eaf7a69bcb323e3dbf18bd27ab6b3ab8df4
Adrian Czarnous <adrian.czarnous@infermedica.com>
2023-02-25 13:22:13 +0100

    add .env

--- main.py
+++ main.py
@@ -1,32 +1,32 @@
 import requests
 from webcolors import CSS3_HEX_TO_NAMES
+import dotenv
+import os
+
+dotenv.load_dotenv()
 
 
-base_url = "https://api.figma.com/v1/files/w4PzT8VxwxmeBsUzZgqxsO/"
+base_url = f"https://api.figma.com/v1/files/{os.environ.get('FIGMA_FILE_NAME')}/"
 
-api_key = "figd_zomKpO19BWGLMBuhe08C8QBdIf34yfdiFW2XJSQZ"
 
-params = {"depth": 1}
-
-headers = {"X-Figma-Token": api_key}
+headers = {"X-Figma-Token": os.environ.get("FIGMA_API_KEY")}
 
 font_families = set()
 font_weights = set()
 letter_spacing = set()
 colors_set = []
 shadows = []
-# https://api.figma.com/v1/files/w4PzT8VxwxmeBsUzZgqxsO/nodes?ids=1825:3149&depth=1
 
 
 def set_shadows(effects):
     for effect in effects:
-        print(effect)
+        pass
 
 
 def set_colors(colors):
     global colors_set
-    color = [colors[hue] * 255 for hue in colors]
-    if not color is colors_set:
+    color = {hue: colors[hue] * 255 for hue in colors}
+    if not color in colors_set:
         colors_set.append(color)
 
 
@@ -35,11 +35,10 @@ def set_fonts(fonts):
     global font_weights
     font_families.add(fonts["fontFamily"])
     font_weights.add(fonts["fontWeight"])
-    letter_spacing.add(fonts["letterSpacing"])
+    letter_spacing.add(round(fonts["letterSpacing"], 2))
 
 
 def set_properties(children):
-    global colors_set
     for child in children:
         nodes = child.get("children")
 
@@ -72,3 +71,27 @@ else:
     data = response.json()
     children = data["document"]["children"]
     set_properties(children)
+
+
+with open("root.scss", mode="w") as file:
+    fw = [f"--font-weight-{w}: {w}; \n" for w in sorted(font_weights)]
+    ff = [f"--font-{f.lower()}: '{f}'; \n" for f in font_families]
+    ls = [
+        f"--letter-spacing-{(index + 1) * 10}: {f}px; \n"
+        for index, f in enumerate(sorted(letter_spacing))
+    ]
+    file.write(":root { \n")
+    file.write("\n")
+    file.write("// Font Weights \n")
+    file.write("\n")
+    file.writelines(fw)
+    file.write("\n")
+    file.write("// Font Families \n\n")
+    file.write("\n")
+    file.writelines(ff)
+    file.write("\n")
+    file.write("//Letters spacings\n")
+    file.write("\n")
+    file.writelines(ls)
+
+    file.write("}")
