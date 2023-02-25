58b563ab7a6a6b3c35768eaf8b2ba50bf92f257f
Adrian Czarnous <adrian.czarnous@infermedica.com>
2023-02-25 04:22:29 +0100

    add set_fonts func to get font styles value from file

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import requests
+from webcolors import CSS3_HEX_TO_NAMES
+
 
 base_url = "https://api.figma.com/v1/files/w4PzT8VxwxmeBsUzZgqxsO/"
 
@@ -8,12 +10,36 @@ params = {"depth": 1}
 
 headers = {"X-Figma-Token": api_key}
 
-colors = set()
-
+font_families = set()
+font_weights = set()
+letter_spacing = set()
+colors_set = []
+shadows = []
 # https://api.figma.com/v1/files/w4PzT8VxwxmeBsUzZgqxsO/nodes?ids=1825:3149&depth=1
 
 
+def set_shadows(effects):
+    for effect in effects:
+        print(effect)
+
+
+def set_colors(colors):
+    global colors_set
+    color = [colors[hue] * 255 for hue in colors]
+    if not color is colors_set:
+        colors_set.append(color)
+
+
+def set_fonts(fonts):
+    global font_families
+    global font_weights
+    font_families.add(fonts["fontFamily"])
+    font_weights.add(fonts["fontWeight"])
+    letter_spacing.add(fonts["letterSpacing"])
+
+
 def set_properties(children):
+    global colors_set
     for child in children:
         nodes = child.get("children")
 
@@ -22,13 +48,13 @@ def set_properties(children):
         effects = child.get("effects")
 
         if colors:
-            pass
+            set_colors(colors)
 
         if type_style:
-            pass
+            set_fonts(type_style)
 
         if effects:
-            print(effects)
+            set_shadows(effects)
 
         if nodes and len(nodes):
             set_properties(nodes)
