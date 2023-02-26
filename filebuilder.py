a84379123de03415a5ea8b1dfd6ec014c00e7fd5
Adrian Czarnous <adrian.czarnous@infermedica.com>
2023-02-26 16:58:44 +0100

    create reset file

--- filebuilder.py
+++ filebuilder.py
@@ -1,35 +1,70 @@
 from io import TextIOWrapper
+import os
 from fetcher import Fetcher
+from fetchdata import fetch_reset
 
 
 class FileBuilder:
-    def __init__(self, scraper: Fetcher):
-        self.scraper = scraper
+    def __init__(self, fetcher: Fetcher):
+        self.fetcher = fetcher
         self.colors = []
         self.create_colors()
 
         self.fw = [
-            f"--font-weight-{w}: {w}; \n" for w in sorted(self.scraper.font_weights)
+            f"--font-weight-{w}: {w}; \n" for w in sorted(self.fetcher.font_weights)
         ]
         self.ff = [
             f"--font-{f.replace(' ', '-').lower()}: '{f}'; \n"
-            for f in self.scraper.font_families
+            for f in self.fetcher.font_families
         ]
         self.ls = [
             f"--letter-spacing-{self.set_variables_name_by_index(index)}: {f}px; \n"
-            for index, f in enumerate(sorted(self.scraper.letter_spacing))
+            for index, f in enumerate(sorted(self.fetcher.letter_spacing))
         ]
         self.fs = [
             f"--fs-{self.set_variables_name_by_index(index)}: {round((fs / 16), 3)}rem; \n"
-            for index, fs in enumerate(sorted(self.scraper.font_sizes))
+            for index, fs in enumerate(sorted(self.fetcher.font_sizes))
         ]
 
+        try:
+            os.mkdir("scss")
+        except FileExistsError:
+            pass
+
     def build_root_file(self):
-        with open("root.scss", mode="w") as file:
+        with open("scss/root.scss", mode="w") as file:
             self.write_file(file)
+
+    def build_utilities(self):
+        with open("scss/utilities.scss", mode="w") as file:
+            text_colors = []
+            background_colors = []
+            font_sizes = []
+
+            for color in self.colors:
+                text_color = self.create_colors_utilities(color, "clr", "text", "color")
+                background_color = self.create_colors_utilities(
+                    color, "clr", "bg", "background-color"
+                )
+                text_colors.append(text_color)
+                background_colors.append(background_color)
+
+            for font in self.fs:
+                font = self.create_colors_utilities(font, "fs", "fs", "font-size")
+                font_sizes.append(font)
+
+            file.write("@forward 'root'; \n\n")
+            file.writelines(text_colors)
+            file.writelines(background_colors)
+            file.writelines(font_sizes)
 
     def set_variables_name_by_index(self, index: int):
         return (index + 1) * 100
+
+    def create_reset_scss(self):
+        with open("scss/reset.scss", mode="w") as file:
+            content = fetch_reset()
+            file.writelines(content)
 
     def write_file(self, file: TextIOWrapper):
         file.write(":root { \n")
@@ -51,7 +86,7 @@ class FileBuilder:
         file.write("}")
 
     def create_colors(self):
-        for color in sorted(self.scraper.colors, key=lambda c: c["name"]):
+        for color in sorted(self.fetcher.colors, key=lambda c: c["name"]):
             color_name = f"--clr-{color['name'].replace('_', '-').lower()}"
             value = f": rgba({color['color'][0]}, {color['color'][1]}, {color['color'][2]}, {color['color'][3]}); \n"
 
@@ -74,3 +109,13 @@ class FileBuilder:
                 )
 
             self.colors.append(color_name + value)
+
+    def create_colors_utilities(
+        self, color: str, css_prefix: str, prefix: str, property: str
+    ):
+        css_variable = color.split(":")[0]
+        class_name = css_variable.split(f"--{css_prefix}-")[1]
+        class_content = (
+            f".{prefix}-{class_name} {{\n   {property}: var({css_variable}); \n}}\n\n"
+        )
+        return class_content
--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import gc
 
 fb = FileBuilder(Fetcher())
 
+fb.create_reset_scss()
 fb.build_root_file()
+fb.build_utilities()
 
 gc.collect()
 print(len(gc.get_objects()))
