781a9d40feb1a4e2065896781f77b657a5d1be40
Adrian Czarnous <adrian.czarnous@infermedica.com>
2023-02-26 14:59:15 +0100

    add pallete color to project

--- file_builder.py
+++ file_builder.py
@@ -1,10 +1,13 @@
 from io import TextIOWrapper
 from scraper import Scraper
+import re
 
 
 class FileBuilder:
     def __init__(self, scraper: Scraper):
         self.scraper = scraper
+        self.colors = []
+        self.create_colors()
 
         self.blue_clr = self.create_colors_variables(
             colors=self.scraper.blue_colors, color_name="blue"
@@ -25,13 +28,16 @@ class FileBuilder:
         self.fw = [
             f"--font-weight-{w}: {w}; \n" for w in sorted(self.scraper.font_weights)
         ]
-        self.ff = [f"--font-{f.lower()}: '{f}'; \n" for f in self.scraper.font_families]
+        self.ff = [
+            f"--font-{f.replace(' ', '-').lower()}: '{f}'; \n"
+            for f in self.scraper.font_families
+        ]
         self.ls = [
             f"--letter-spacing-{self.set_variables_name_by_index(index)}: {f}px; \n"
             for index, f in enumerate(sorted(self.scraper.letter_spacing))
         ]
         self.fs = [
-            f"--fs-{self.set_variables_name_by_index(index)}: {fs}px; \n"
+            f"--fs-{self.set_variables_name_by_index(index)}: {round((fs / 16), 3)}rem; \n"
             for index, fs in enumerate(sorted(self.scraper.font_sizes))
         ]
 
@@ -69,20 +75,39 @@ class FileBuilder:
         file.writelines(self.fs)
         file.write("\n")
         file.write("//Colors \n")
-        file.write("//Blue \n")
-        file.writelines(self.blue_clr)
-        file.write("\n")
-        file.write("//Gray \n")
-        file.writelines(self.gray_clr)
-        file.write("\n")
-        file.write("//Red \n")
-        file.writelines(self.red_clr)
-        file.write("\n")
-        file.write("//Green \n")
-        file.writelines(self.green_clr)
+        file.writelines(self.colors)
         file.write("\n")
         file.writelines(self.ls)
 
         file.write("}")
 
-        return
+    def create_colors(self):
+        for color in sorted(self.scraper.colors, key=lambda c: c["name"]):
+            color_name = f"--clr-{color['name'].replace('_', '-').lower()}"
+            value = f": rgba({color['color'][0]}, {color['color'][1]}, {color['color'][2]}, {color['color'][3]}); \n"
+
+            def check(string, color_name):
+                if color_name in string:
+                    return True
+
+                return False
+
+            color_with_same_name = list(
+                filter(lambda x: check(x, color_name=color_name), self.colors)
+            )
+
+            if len(color_with_same_name):
+                color_index = "".join(char for char in color_name if char.isdigit())
+
+                color_name = color_name.replace(
+                    str(color_index),
+                    str(int(color_index) + len(color_with_same_name) * 100),
+                )
+
+            self.colors.append(color_name + value)
+            self.colors = list(dict.fromkeys(self.colors))
+
+        #  self.colors = [
+        #     f"--clr-{color['name'].replace('_', '-').lower()}: rgba({color['color'][0]}, {color['color'][1]}, {color['color'][2]}, {color['color'][3]}  ); \n"
+        #     for color in sorted(self.scraper.colors, key=lambda color: color["name"])
+        # ]
